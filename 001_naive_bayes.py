5d220fc27feb57e581850bde452d8b7695d359b5
mhassantrq <mhassantrq@gmail.com>
2026-05-13 19:40:44 +0500

    naive bayes, calculated vocab and prior probs

--- 001_naive_bayes.py
+++ 001_naive_bayes.py
@@ -3,12 +3,26 @@ Naive Bayes from scratch
 """
 
 import pandas as pd
-
+from collections import defaultdict
 
 df = pd.read_csv('data/dataset01.csv')
 df['tag'] = df['tag'].map({'positive': 1, 'negative': 0})
 
+vocab = defaultdict(int)
+for_vocab = ' '.join(df['text'].astype(str).values.flatten()).split(' ')
 
+for word in for_vocab:
+    vocab[word] += 1
 
+print(f'Total Words at start: {len(for_vocab)}')
+print(f'Words in vocabulary without preprocessing: {len(vocab)}')
 
-print(df)
+neg_count = (df['tag'] == 0).sum()
+pos_count = (df['tag'] == 1).sum()
+
+print(f'Negative Sentiments: {neg_count}, Positive Sentiments: {pos_count}')
+
+neg_prior = neg_count / (neg_count+pos_count)
+pos_prior = pos_count / (neg_count+pos_count)
+
+print(f'neg prior probability: {neg_prior}, pos prior probability: {pos_prior}')
