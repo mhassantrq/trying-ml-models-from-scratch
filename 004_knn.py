2a0061697a486cee390d75b23149945e79e2472f
mhassantrq <mhassantrq@gmail.com>
2026-05-21 21:41:34 +0500

    finding nearest points distance

--- 004_knn.py
+++ 004_knn.py
@@ -3,8 +3,7 @@ knn from scratch
 """
 
 import math
-
-k=3
+import matplotlib.pyplot as plt
 
 def euclidean_dist(x,y):
     sum=0
@@ -12,3 +11,26 @@ def euclidean_dist(x,y):
         sum += (x[i] - y[i])**2
     return math.sqrt(sum)
 
+points = {
+    'group1': [[3,5], [4,5], [3,6], [2,6]],
+    'group2': [[7,3], [5,5], [7,5], [7,6], [6,3]],
+}
+
+x1,y1 = zip(*points['group1'])
+x2,y2 = zip(*points['group2'])
+
+plt.scatter(x1,y1)
+plt.scatter(x2,y2)
+
+new_point = [3,3]
+
+plt.scatter(3,3)
+plt.show()
+
+nn_dist = []
+for group in points:
+    for point in points[group]:
+        dist = euclidean_dist(point, new_point)
+        nn_dist.append([dist, group])
+
+print(sorted(nn_dist))
