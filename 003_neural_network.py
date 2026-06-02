27f142df1d9c7eb43d892a94c43f42b00e308210
mhassantrq <mhassantrq@gmail.com>
2026-06-02 21:25:40 +0500

    updating

--- 003_neural_network.py
+++ 003_neural_network.py
@@ -8,53 +8,25 @@ import random
 import numpy as np
 
 class NeuralNetwork:
-    pass
+    def three_layer(self):
+        X = [[random.random() for _ in range(3)],
+            [random.random() for _ in range(3)],
+            [random.random() for _ in range(3)]]
 
-class Neuron:
-    def single_neuron_manual(self):
+        w1 = [[random.random() for _ in range(3)],
+            [random.random() for _ in range(3)]]
+        b1 = [random.random() for _ in range(2)]
 
-        x = [0.2,2,3]
-        w = [-0.35,0.5,0.9]
-        b = -0.23
-        res = 0
+        w2 = [[random.random() for _ in range(3)],
+            [random.random() for _ in range(3)]]
+        b2 = [random.random() for _ in range(2)]
 
-        for i in range(3):
-            res += x[i]*w[i]
+        output1 = np.dot(X, np.array(w1).T) + b1
+        output1 = np.dot(X, np.array(w1).T) + b1
 
-        res += b
-        res = 1 / (1 + 2.718 ** -res)
-
-        print(res)
-
-    def single_neuron_numpy(self):
-
-        x = [0.2,2,3]
-        w = [-0.35,0.5,0.9]
-        b = -0.23
-        res = np.dot(w,x)
-        res += b
-        res = 1 / (1 + 2.718 ** -res)
-
-        print(res)
-
-    def single_layer(self):
-        X = [0.35, -0.6, 1.2]
-        w = [[0.2, 1.1, 0.7],
-             [1.9, 0.65, -0.53]]
-        b = [0.9, 0.85]
-        outputs = np.dot(w, X) + b
-        print(outputs)
-
-    def single_layer_multiple_samples(self):
-        X = [[0.35, -0.6, 1.2],
-             [0.39, 0.5, 0.9],
-             [-1.2, -1.3, 0.66]]
-        w = [[0.2, 1.1, 0.7],
-             [1.9, 0.65, -0.53]]
-        b = [0.9, 0.85]
-        outputs = np.dot(X, np.array(w).T) + b
-        print(outputs)
+#        print(outputs)
+#   pending...
 
 
-n = Neuron()
-n.single_layer_multiple_samples()
+n = NeuralNetwork()
+n.double_layer()
