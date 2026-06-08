549422fdcb9741889bb49a6c09fa38ecec136f0d
mhassantrq <mhassantrq@gmail.com>
2026-06-08 20:00:08 +0500

    added weights, bias and feed forward

--- 003_neural_network.py
+++ 003_neural_network.py
@@ -10,7 +10,9 @@ import math
 
 class NeuralNetwork:
     def three_layer(self):
-        X = [random.random() for _ in range(3)]
+        input = [[random.random() for _ in range(3)],
+             [random.random() for _ in range(3)],
+             [random.random() for _ in range(3)]]
 
         w1 = [[random.random() for _ in range(3)],
             [random.random() for _ in range(3)]]
@@ -23,15 +25,34 @@ class NeuralNetwork:
         w3 = [random.random() for _ in range(2)]
         b3 = [random.random()]
 
-        output1 = np.dot(X, np.array(w1).T) + b1
+        output1 = np.dot(input, np.array(w1).T) + b1
         output2 = np.dot(output1, np.array(w2).T) + b2
         output3 = np.dot(output2, np.array(w3).T) + b3
 
         print(output1)
         print(output2)
         print(output3)
-        sig = 1/(1+math.exp(-output3[0]))
-        print(f'Sigmoid: {sig}')
+        sig1 = 1/(1+math.exp(-output3[0]))
+        sig2 = 1/(1+math.exp(-output3[1]))
+        sig3 = 1/(1+math.exp(-output3[2]))
+
+        print(f'Sigmoid: {sig1, sig2, sig3}')
+
+    def initialize_weights_bias(self, neurons, inputs):
+        self.weights = [[random.random() for _ in range(neurons)]
+                        for _ in range(inputs)]
+        self.bias = [random.random() for _ in range(neurons)]
+        print(self.weights)
+        print(self.bias)
+
+    def feed_forward(self, input):
+        self.y = np.dot(input, self.weights) + self.bias
+        print(self.y)
 
 n = NeuralNetwork()
-n.three_layer()
+#n.three_layer()
+n.initialize_w_b(2, 3)
+
+
+input = [random.random() for _ in range(3)]
+n.feed_forward(input)
